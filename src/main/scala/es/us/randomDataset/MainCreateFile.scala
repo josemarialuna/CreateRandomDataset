267ef46efe63c069ad2958bd121f49dffe3f3953
José María Luna <josemarialunaromera@gmail.com>
2018-02-01 11:23:46 +0100

    Final commit

--- src/main/scala/es/us/randomDataset/MainCreateFile.scala
+++ src/main/scala/es/us/randomDataset/MainCreateFile.scala
@@ -20,82 +20,17 @@ object MainCreateFile {
 
     val sc = new SparkContext(conf)
 
-    var dimensions = 20
+    var dimensions = 3
     var clusters = 5
-    var instances = 1000
-    var desvTip = 0.05f
+    var instances = 100
+    var standDev = 0.05f
 
-    if (args.length > 2) {
-      dimensions = args(0).toInt
-      clusters = args(1).toInt
-      instances = args(2).toInt
-      desvTip = args(3).toFloat
-    }
-
-    val valor = scala.math.pow(2, dimensions).toInt
-    var marcados = 0
-    var marcadosArray = Array.fill(valor) {
-      0
-    }
-
-    var indicesSelected = Array[Int]()
-
-    while (marcados < clusters) {
-      val randomNumber = getRandom(0, valor - 1).toInt
-      if (marcadosArray(randomNumber) == 0) {
-        indicesSelected = indicesSelected :+ randomNumber
-        marcadosArray(randomNumber) = 1
-        marcados += 1
-      }
-    }
-    println(indicesSelected.mkString(";"))
-    val binarySelected = indicesSelected.map(toBinary(_, dimensions))
-    println(binarySelected.mkString(";"))
-
-    val indices = binarySelected
-      .map(x => x.toArray.map(giveMeNumber(_)))
-
-    val indicesRDD = sc.parallelize(indices).cache()
-
-    val dataset = indicesRDD.flatMap(x =>
-      for {i <- 0 until instances} yield {
-        dimensions match {
-          case 2 => (getGaussian(x(0), desvTip), getGaussian(x(1), desvTip))
-          case 3 => (getGaussian(x(0), desvTip), getGaussian(x(1), desvTip), getGaussian(x(2), desvTip))
-          case 4 => (getGaussian(x(0), desvTip), getGaussian(x(1), desvTip), getGaussian(x(2), desvTip), getGaussian(x(3), desvTip))
-          case 5 => (getGaussian(x(0), desvTip), getGaussian(x(1), desvTip), getGaussian(x(2), desvTip), getGaussian(x(3), desvTip), getGaussian(x(4), desvTip))
-          case 6 => (getGaussian(x(0), desvTip), getGaussian(x(1), desvTip), getGaussian(x(2), desvTip), getGaussian(x(3), desvTip), getGaussian(x(4), desvTip), getGaussian(x(5), desvTip))
-          case 7 => (getGaussian(x(0), desvTip), getGaussian(x(1), desvTip), getGaussian(x(2), desvTip), getGaussian(x(3), desvTip), getGaussian(x(4), desvTip), getGaussian(x(5), desvTip), getGaussian(x(6), desvTip))
-          case 8 => (getGaussian(x(0), desvTip), getGaussian(x(1), desvTip), getGaussian(x(2), desvTip), getGaussian(x(3), desvTip), getGaussian(x(4), desvTip), getGaussian(x(5), desvTip), getGaussian(x(6), desvTip), getGaussian(x(7), desvTip))
-          case 9 => (getGaussian(x(0), desvTip), getGaussian(x(1), desvTip), getGaussian(x(2), desvTip), getGaussian(x(3), desvTip), getGaussian(x(4), desvTip), getGaussian(x(5), desvTip), getGaussian(x(6), desvTip), getGaussian(x(7), desvTip), getGaussian(x(8), desvTip))
-          case 10 => (getGaussian(x(0), desvTip), getGaussian(x(1), desvTip), getGaussian(x(2), desvTip), getGaussian(x(3), desvTip), getGaussian(x(4), desvTip), getGaussian(x(5), desvTip), getGaussian(x(6), desvTip), getGaussian(x(7), desvTip), getGaussian(x(8), desvTip), getGaussian(x(9), desvTip))
-          case 20 => new Dim20(getGaussian(x(0), desvTip), getGaussian(x(1), desvTip), getGaussian(x(2), desvTip), getGaussian(x(3), desvTip), getGaussian(x(4), desvTip), getGaussian(x(5), desvTip), getGaussian(x(6), desvTip), getGaussian(x(7), desvTip), getGaussian(x(8), desvTip), getGaussian(x(9), desvTip), getGaussian(x(10), desvTip), getGaussian(x(11), desvTip), getGaussian(x(12), desvTip), getGaussian(x(13), desvTip), getGaussian(x(14), desvTip), getGaussian(x(15), desvTip), getGaussian(x(16), desvTip), getGaussian(x(17), desvTip), getGaussian(x(18), desvTip), getGaussian(x(19), desvTip))
-        }
-      }
-    )
-
-    println(dataset.count())
-
-    dataset.map(_.toString).coalesce(1).saveAsTextFile(s"C$clusters-D$dimensions-I$instances-${Utils.whatTimeIsIt()}")
+    RandomDataset.createFile(sc, dimensions, clusters, instances, standDev, "")
 
     sc.stop()
   }
 
-  def getRandom(from: Integer, to: Integer): Float = {
-    val rnd = new Random()
-    rnd.nextInt((to - from) + 1) + from
-  }
 
-  def getGaussian(average: Float, desv: Float): Float = {
-    val rnd = new Random()
-    rnd.nextGaussian().toFloat * desv + average
-  }
-
-  def toBinary(i: Int, digits: Int = 8) =
-    String.format("%" + digits + "s", i.toBinaryString).replace(' ', '0')
-
-  def giveMeNumber(i: Int): Float =
-    if (i == 48) 0.25f else 0.75f
 
 
 }
---
+++ src/main/scala/es/us/randomDataset/RandomDataset.scala
@@ -0,0 +1,92 @@
+package es.us.randomDataset
+
+import es.us.randomDataset.MainCreateFile.{getGaussian, getRandom, giveMeNumber, toBinary}
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+import scala.util.Random
+
+/**
+  * Created by Josem on 13/06/2017.
+  */
+object RandomDataset {
+  def createFile(sc: SparkContext, features: Int, clusters: Int, instances: Int, standDev: Float, path: String) = {
+
+
+    val valor = scala.math.pow(2, features).toInt
+    var marcados = 0
+    var marcadosArray = Array.fill(valor) {
+      0
+    }
+
+    var indicesSelected = Array[Int]()
+
+    while (marcados < clusters) {
+      val randomNumber = getRandom(0, valor - 1).toInt
+      if (marcadosArray(randomNumber) == 0) {
+        indicesSelected = indicesSelected :+ randomNumber
+        marcadosArray(randomNumber) = 1
+        marcados += 1
+      }
+    }
+
+    println(indicesSelected.mkString(";"))
+    val binarySelected = indicesSelected.map(toBinary(_, features))
+    println(binarySelected.mkString(";"))
+
+    val indices = binarySelected
+      .map(x => x.toArray.map(giveMeNumber(_)))
+
+    val indicesRDD = sc.parallelize(indices).cache()
+
+    val dataset = indicesRDD.flatMap(x =>
+      for {i <- 0 until instances} yield {
+        features match {
+          case 2 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev))
+          case 3 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev))
+          case 4 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev))
+          case 5 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev))
+          case 6 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev))
+          case 7 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev))
+          case 8 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev))
+          case 9 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev))
+          case 10 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev), getGaussian(x(9), standDev))
+          case 11 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev), getGaussian(x(9), standDev), getGaussian(x(10), standDev))
+          case 12 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev), getGaussian(x(9), standDev), getGaussian(x(10), standDev), getGaussian(x(11), standDev))
+          case 13 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev), getGaussian(x(9), standDev), getGaussian(x(10), standDev), getGaussian(x(11), standDev), getGaussian(x(12), standDev))
+          case 14 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev), getGaussian(x(9), standDev), getGaussian(x(10), standDev), getGaussian(x(11), standDev), getGaussian(x(12), standDev), getGaussian(x(13), standDev))
+          case 15 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev), getGaussian(x(9), standDev), getGaussian(x(10), standDev), getGaussian(x(11), standDev), getGaussian(x(12), standDev), getGaussian(x(13), standDev), getGaussian(x(14), standDev))
+          case 16 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev), getGaussian(x(9), standDev), getGaussian(x(10), standDev), getGaussian(x(11), standDev), getGaussian(x(12), standDev), getGaussian(x(13), standDev), getGaussian(x(14), standDev), getGaussian(x(15), standDev))
+          case 17 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev), getGaussian(x(9), standDev), getGaussian(x(10), standDev), getGaussian(x(11), standDev), getGaussian(x(12), standDev), getGaussian(x(13), standDev), getGaussian(x(14), standDev), getGaussian(x(15), standDev), getGaussian(x(16), standDev))
+          case 18 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev), getGaussian(x(9), standDev), getGaussian(x(10), standDev), getGaussian(x(11), standDev), getGaussian(x(12), standDev), getGaussian(x(13), standDev), getGaussian(x(14), standDev), getGaussian(x(15), standDev), getGaussian(x(16), standDev), getGaussian(x(17), standDev))
+          case 19 => (getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev), getGaussian(x(9), standDev), getGaussian(x(10), standDev), getGaussian(x(11), standDev), getGaussian(x(12), standDev), getGaussian(x(13), standDev), getGaussian(x(14), standDev), getGaussian(x(15), standDev), getGaussian(x(16), standDev), getGaussian(x(17), standDev), getGaussian(x(18), standDev))
+          case 20 => new Dim20(getGaussian(x(0), standDev), getGaussian(x(1), standDev), getGaussian(x(2), standDev), getGaussian(x(3), standDev), getGaussian(x(4), standDev), getGaussian(x(5), standDev), getGaussian(x(6), standDev), getGaussian(x(7), standDev), getGaussian(x(8), standDev), getGaussian(x(9), standDev), getGaussian(x(10), standDev), getGaussian(x(11), standDev), getGaussian(x(12), standDev), getGaussian(x(13), standDev), getGaussian(x(14), standDev), getGaussian(x(15), standDev), getGaussian(x(16), standDev), getGaussian(x(17), standDev), getGaussian(x(18), standDev), getGaussian(x(19), standDev))
+        }
+      }
+    )
+
+    println(dataset.count())
+
+    dataset.map(_.toString).coalesce(1).saveAsTextFile(s"C$clusters-D$features-I$instances-${Utils.whatTimeIsIt()}")
+
+
+  }
+
+  def getRandom(from: Integer, to: Integer): Float = {
+    val rnd = new Random()
+    rnd.nextInt((to - from) + 1) + from
+  }
+
+  def getGaussian(average: Float, desv: Float): Float = {
+    val rnd = new Random()
+    rnd.nextGaussian().toFloat * desv + average
+  }
+
+  def toBinary(i: Int, digits: Int = 8) =
+    String.format("%" + digits + "s", i.toBinaryString).replace(' ', '0')
+
+  def giveMeNumber(i: Int): Float =
+    if (i == 48) 0.25f else 0.75f
+
+
+}
